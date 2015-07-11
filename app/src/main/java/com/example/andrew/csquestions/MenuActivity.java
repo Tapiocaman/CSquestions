da473a7c27b72921b530e09df80f72c70520f381
Tapiocaman <acore@uci.edu>
2015-07-10 21:01:45 -0700

    Initial commit

---
+++ app/src/main/java/com/example/andrew/csquestions/MenuActivity.java
@@ -0,0 +1,91 @@
+package com.example.andrew.csquestions;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+
+public class MenuActivity extends Activity {
+
+    private static final int SWIPE_THRESHOLD = 100;
+    private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        boolean result = false;
+        try {
+            float diffY = e2.getY() - e1.getY();
+            float diffX = e2.getX() - e1.getX();
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffX > 0) {
+                        onSwipeRight();
+                    }
+                    else {
+                        onSwipeLeft();
+                    }
+                }
+                result = true;
+            }
+            else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                if (diffY > 0) {
+                    onSwipeBottom();
+                }
+            }
+            result = true;
+
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return result;
+    }
+
+    private void onSwipeRight(){
+
+    }
+    private void onSwipeLeft(){
+
+    }
+    private void onSwipeBottom(){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/andrew/csquestions/QuestionActivity.java
@@ -0,0 +1,16 @@
+package com.example.andrew.csquestions;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by andrew on 6/30/15.
+ */
+public class QuestionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
