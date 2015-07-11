276b016406475b39f3587775e60505ff0d3abe2d
Tapiocaman <acore@uci.edu>
2015-07-10 21:22:59 -0700

    Added swipe functionality

--- app/src/main/java/com/example/andrew/csquestions/MenuActivity.java
+++ app/src/main/java/com/example/andrew/csquestions/MenuActivity.java
@@ -10,6 +10,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
 
 
 public class MenuActivity extends Activity {
@@ -45,47 +47,48 @@ public class MenuActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        boolean result = false;
-        try {
-            float diffY = e2.getY() - e1.getY();
-            float diffX = e2.getX() - e1.getX();
-            if (Math.abs(diffX) > Math.abs(diffY)) {
-                if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                    if (diffX > 0) {
-                        onSwipeRight();
+    private OnGestureListener mOnGesture = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
+                        }
                     }
-                    else {
-                        onSwipeLeft();
+                    result = true;
+                } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom();
                     }
                 }
                 result = true;
-            }
-            else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                if (diffY > 0) {
-                    onSwipeBottom();
-                }
-            }
-            result = true;
 
-        }
-        catch (Exception exception) {
-            exception.printStackTrace();
-        }
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
 
-        return result;
-    }
+            return result;
+        }
+    };
 
     private void onSwipeRight(){
-
+        Intent intent = new Intent(this, QuestionActivity.class);
+        startActivity(intent);
     }
     private void onSwipeLeft(){
-
+        Intent intent = new Intent(this, QuestionActivity.class);
+        startActivity(intent);
     }
     private void onSwipeBottom(){
-
+        Intent intent = new Intent(this, QuestionActivity.class);
+        startActivity(intent);
     }
 
 }
